26df10a9e7732ee7c97b9cad98f4d40ac1d788f6
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2014-04-21 21:56:27 -0500

    Initial commit, fresh out of gdxsetup.jar

---
+++ desktop/src/lando/systems/ld29/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package lando.systems.ld29.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import lando.systems.ld29.LudumDare29;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LudumDare29(), config);
+	}
+}
---
+++ html/src/lando/systems/ld29/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package lando.systems.ld29.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import lando.systems.ld29.LudumDare29;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new LudumDare29();
+        }
+}
