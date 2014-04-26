a41364f72b4d7a1e248f07808d908cab76f90b18
Colin <moshen.colin@gmail.com>
2014-04-26 18:24:04 -0500

    Adds 5 Block types, Random Block method

---
+++ core/src/lando/systems/ld29/blocks/DirtBlock.java
@@ -0,0 +1,15 @@
+package lando.systems.ld29.blocks;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import lando.systems.ld29.core.Assets;
+
+public class DirtBlock extends Block {
+    private static TextureRegion img = Assets.blocks.get("dirt");
+
+    public DirtBlock(float x, float y) {
+        super(x, y);
+
+        setSprite(new Sprite(img));
+    }
+}
---
+++ core/src/lando/systems/ld29/blocks/IronBlock.java
@@ -0,0 +1,15 @@
+package lando.systems.ld29.blocks;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import lando.systems.ld29.core.Assets;
+
+public class IronBlock extends Block {
+    private static TextureRegion img = Assets.blocks.get("iron");
+
+    public IronBlock(float x, float y) {
+        super(x, y);
+
+        setSprite(new Sprite(img));
+    }
+}
