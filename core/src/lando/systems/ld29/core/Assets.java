591414aa99327eb9d9ad03528845cf2ed047b4ef
Colin <moshen.colin@gmail.com>
2014-04-26 16:29:25 -0500

    Create sprite-sheet of resources, added to Assets

--- core/src/lando/systems/ld29/core/Assets.java
+++ core/src/lando/systems/ld29/core/Assets.java
@@ -6,8 +6,11 @@ import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.Texture.*;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 public class Assets {
@@ -16,6 +19,7 @@ public class Assets {
     public static ShapeRenderer shapes;
 
     public static Texture libgdx;
+    public static Map<String,TextureRegion> resources;
 
     //public static Sound sound;
     //public static Music music;
@@ -27,6 +31,17 @@ public class Assets {
 
         libgdx = new Texture("badlogic.jpg");
         libgdx.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+
+        Texture resourcesSpritesheet = new Texture("art/resources-spritesheet.png");
+        resources = new HashMap<String,TextureRegion>();
+        resources.put("dirt"  ,new TextureRegion(resourcesSpritesheet, 0    , 0, 16, 16));
+        resources.put("acorn" ,new TextureRegion(resourcesSpritesheet, 16   , 0, 16, 16));
+        resources.put("gold"  ,new TextureRegion(resourcesSpritesheet, 32   , 0, 16, 16));
+        resources.put("grapes",new TextureRegion(resourcesSpritesheet, 48   , 0, 16, 16));
+        resources.put("iron"  ,new TextureRegion(resourcesSpritesheet, 64   , 0, 16, 16));
+        resources.put("stone" ,new TextureRegion(resourcesSpritesheet, 80   , 0, 16, 16));
+        resources.put("lava"  ,new TextureRegion(resourcesSpritesheet, 96   , 0, 16, 16));
+        resources.put("water" ,new TextureRegion(resourcesSpritesheet, 112  , 0, 16, 16));
 
 //        sound = Gdx.audio.newSound("audio/sound.wav");
 //        music = Gdx.audio.newMusic("audio/music.mp3");
